fdf055e94cb89387df6415992d585500923d949b
Antoine <antoine@crafting-labs.fr>
2013-09-10 23:36:02 +0200

    correct some typos

--- src/PebbleTextWatch-fr.c
+++ src/PebbleTextWatch-fr.c
@@ -36,12 +36,12 @@ static const char* const HOURS[] = {
 };
 
 static const char* const MINUTES[] = {
-  "",          "un",              "deux",           "trois",          "quatre",           "cinq",           "six",           "sept",           "huit",           "neuf",
-  "dix",       "onze",            "douze",          "treize",         "quatorze",         "quinze",         "seize",         "dix-sept",       "dix-huit",       "dix-neuf",
-  "vingt",     "vingt-et-un",     "ving-deux",      "vingt-trois",    "vingt-quatre",     "vingt-cinq",     "vingt-six",     "vingt-sept",     "vingt-huit",     "vingt-neuf",
-  "trente",    "trente-et-un",    "trente-deux",    "trente-trois",   "trente-quatre",    "trente-cinq",    "trente-six",    "trente-sept",    "trente-huit",    "trente-neuf",
-  "quarante",  "quarante-et-un",  "quarante-deux",  "quarante-trois", "quarante-quatre",  "quarante-cinq",  "quarante-six",  "quarante-sept",  "quarante-huit",  "quarante-neuf",
-  "cinquante", "cinquante-et-un", "cinquante-deux", "cinquante-trois","cinquante-quatre", "cinquante-cinq", "cinquante-six", "cinquante-sept", "cinquante-huit", "cinquante-neuf"
+  "",          "un",              "deux",           "trois",           "quatre",           "cinq",           "six",           "sept",           "huit",           "neuf",
+  "dix",       "onze",            "douze",          "treize",          "quatorze",         "quinze",         "seize",         "dix-sept",       "dix-huit",       "dix-neuf",
+  "vingt",     "vingt-et-une",     "vingt-deux",     "vingt-trois",     "vingt-quatre",     "vingt-cinq",     "vingt-six",     "vingt-sept",     "vingt-huit",     "vingt-neuf",
+  "trente",    "trente-et-une",    "trente-deux",    "trente-trois",    "trente-quatre",    "trente-cinq",    "trente-six",    "trente-sept",    "trente-huit",    "trente-neuf",
+  "quarante",  "quarante-et-une",  "quarante-deux",  "quarante-trois",  "quarante-quatre",  "quarante-cinq",  "quarante-six",  "quarante-sept",  "quarante-huit",  "quarante-neuf",
+  "cinquante", "cinquante-et-une", "cinquante-deux", "cinquante-trois", "cinquante-quatre", "cinquante-cinq", "cinquante-six", "cinquante-sept", "cinquante-huit", "cinquante-neuf"
 };
 
 void display_time(PblTm *t)
@@ -82,7 +82,7 @@ void handle_init(AppContextRef ctx) {
   text_layer_set_text_color(&minute_layer, GColorWhite);
   text_layer_set_background_color(&minute_layer, GColorClear);
   text_layer_set_text_alignment(&minute_layer, GTextAlignmentLeft);
-  text_layer_set_font(&minute_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));  
+  text_layer_set_font(&minute_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
 
 
   text_layer_init(&time_layer, GRect(0, 125, 144, 30));
